2d32e0e493466d4478029a99ae72467d29aaab74
DevMasteryLabs <develop.devmastery@gmail.com>
2022-07-31 00:35:29 +0100

    Set up simple routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/' component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
